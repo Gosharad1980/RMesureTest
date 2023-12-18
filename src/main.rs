c9c8e1e8e3d8e1c67447cb93d99dc56ed1a7a33a
Moi <Moi@3eb53af06f81da0792505d7a3487544771630632>
2023-12-19 00:11:25 +0100

    Calcul d'une moyenne

--- src/main.rs
+++ src/main.rs
@@ -11,64 +11,11 @@ use RMesure::RMesure;
 
 fn main()
 {
-    let mut U: RMesure = RMesure::loi(12.0, 0.01, 'R');
-    let R: RMesure = RMesure::loi(1000.0, 1000.0*0.05, 'N');
-    let mut I: RMesure = RMesure::zero();
+    let U1: RMesure = RMesure::loi(12.05, 0.01, 'R');
+    let U2: RMesure = RMesure::loi(12.10, 0.01, 'R');
+    let U3: RMesure = RMesure::loi(11.93, 0.01, 'R');
 
-    let a: RMesure = RMesure::loi(0.5, 0.1, 'N');
-    let b: RMesure = RMesure::loi(1.0, 0.1, 'N');
-    let c: RMesure = RMesure::loi(1.1, 0.1, 'N');
-    let d: RMesure = RMesure::loi(1.5, 0.1, 'N');
+    let moyenne = (U1 + U2 + U3) / 3.0;
 
-    U += RMesure::scalaire(2.0);
-
-    I = U / R;
-
-    println!("-U = {}", -U);
-    println!("R = {R}");
-    println!("I = {I}");
-
-    println!("BoxPlot I = {:?}", I.BoxPlot());
-
-
-    println!("\n");
-    println!("a = {a}");
-    println!("b = {b}");
-    println!("c = {c}");
-    println!("d = {d}");
-
-    println!("");
-
-
-    println!("a == d [ FAUX ] => {}", a==d); // significativement diff�rents
-	println!("b == c [ VRAI ] => {}", b==c); // insignificativement diff�rents
-    println!("");
-
-	println!("a != d [ VRAI ] => {}", a!=d); // significativement diff�rents
-	println!("b != c [ FAUX ] => {}", b!=c); // insignificativement diff�rents
-    println!("");
-
-	println!("c < b  [ FAUX ] => {}", c<b); // insignificativement diff�rents
-	println!("a < d  [ VRAI ] => {}", a<d); // a.valeur < d.valeur et a!=d
-	println!("d < a  [ FAUX ] => {}", d<a); // d.valeur > a.valeur et d!=a
-	println!("b < c  [ FAUX ] => {}", b<c); // insignificativement diff�rents
-    println!("");
-
-	println!("a > d  [ FAUX ] => {}", a>d); // a.valeur < d.valeur et a!=d
-	println!("d > a  [ VRAI ] => {}", d>a); // d.valeur > a.valeur et d!=a
-	println!("c > b  [ FAUX ] => {}", c>b); // insignificativement diff�rents
-	println!("b > c  [ FAUX ] => {}", b>c); // insignificativement diff�rents
-    println!("");
-
-	println!("a <= d [ VRAI ] => {}", a<=d); // a.valeur < d.valeur et a!=d
-	println!("d <= a [ FAUX ] => {}", d<=a); // d.valeur > a.valeur et d!=a
-	println!("c <= b [ VRAI ] => {}", c<=b); // insignificativement diff�rents
-	println!("b <= c [ VRAI ] => {}", b<=c); // insignificativement diff�rents
-    println!("");
-
-	println!("a >= d [ FAUX ] => {}", a>=d); // a.valeur < d.valeur et a!=d
-	println!("d >= a [ VRAI ] => {}", d>=a); // d.valeur > a.valeur et d!=a
-	println!("c >= b [ VRAI ] => {}", c>=b); // insignificativement diff�rents
-	println!("b >= c [ VRAI ] => {}", b>=c); // insignificativement diff�rents
-
+    println!("moyenne = {moyenne}");
 }
