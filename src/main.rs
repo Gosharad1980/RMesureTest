54aa401b1fd347c1e6801c3e2c3d14b54a3ccc31
Moi <Moi@3eb53af06f81da0792505d7a3487544771630632>
2024-01-06 00:06:52 +0100

    Bon on a fait des tests avec le filtrage du 1er ordre

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,23 @@ use RMesure::RMesure;
 
 fn main()
 {
-    let     Mes: RMesure = RMesure::loi(12.00, 0.01, 'R');
-    let     Mdl: RMesure = RMesure::loi(00.00,12.0, 'R');
+    let     Mes: RMesure = RMesure::loi(12.00,0.01, 'R');
+    //let mut Out: RMesure = RMesure::loi(00.00,12.0, 'R');
     let mut Out: RMesure = RMesure::from(0.0);
+    let mut Out_zm1: RMesure = Out.clone();
     
-    let K: f64 = 0.382;
-    let mut prop: f64 = 0.0;
+    let Te = 0.04_f64; // 40ms
+    let Tf = 1.0_f64; // 1sec
+    let K: f64 = 1.0_f64 - (-2.0_f64 * 3.14159_f64 * Te/Tf).exp();
 
     println!("Out = {Out}");
+    println!("Mes = {Mes}");
+    println!("K   = {K}");
 
-    for i in 0..100
+    for i in 0..33
     {
-        
-        Out = prop * Mes.clone() + (1.0 - prop) * Mdl.clone();
-        println!("Out[{i}][{prop}] = {Out} --> {}", Out.clone() == Mes.clone());
-        prop += K * (1.0 - prop);
+        Out = K * Mes.clone() + (1.0_f64 - K) * Out_zm1.clone();
+        println!("Out[{i}] = {Out} --> {}", Out.clone() == Mes.clone());
+        Out_zm1 = Out.clone();
     }
 }
