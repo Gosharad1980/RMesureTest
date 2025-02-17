c24a568e708f28000471c86b9f18cf354880e4ce
Gosharad1980 <199524053+Gosharad1980@users.noreply.github.com>
2025-02-17 22:41:22 +0100

    Petit exemple avec des calculs de résistance de filtre RC

--- src/main.rs
+++ src/main.rs
@@ -14,75 +14,72 @@ use RMesure::RMesure as RMesure;
 
 fn sg_square(periode: f64, kTe: f64) -> RMesure
 {
-    if (kTe % periode) < (periode/2.0) { RMesure::loi(12.00,0.01, 'R') }
-    else                               { RMesure::loi(0.00,0.01, 'R')  }
+	if (kTe % periode) < (periode/2.0) { RMesure::loi(12.00,0.01, 'R') }
+	else                               { RMesure::loi(0.00,0.01, 'R')  }
 }
 
 fn test_incertitude_filtrage_prem_ordre()
 {
-    let periode_square: f64 = 4.0_f64;
-    
-    let mut Meas: RMesure = sg_square(periode_square, 0.0);
+	let periode_square: f64 = 4.0_f64;
 
-    let mut Out_eq1: RMesure = RMesure::loi(00.00,0.01, 'R');
-    let mut Out_eq2: RMesure = Out_eq1.clone();
-    let mut Out_eq1_zm1: RMesure = Out_eq1.clone();
-    let mut Out_eq2_zm1: RMesure = Out_eq2.clone();
-    
-    let Te = 0.04_f64; // 40ms
-    let Tf = 1.0_f64; // 1sec
-    let fac: RMesure = RMesure::from(1.0_f64 - (-2.0_f64 * 3.14159_f64 * Te/Tf).exp());
+	let mut Meas: RMesure = sg_square(periode_square, 0.0);
 
+	let mut Out_eq1: RMesure = RMesure::loi(00.00,0.01, 'R');
+	let mut Out_eq2: RMesure = Out_eq1.clone();
+	let mut Out_eq1_zm1: RMesure = Out_eq1.clone();
+	let mut Out_eq2_zm1: RMesure = Out_eq2.clone();
 
-    println!("Out = {Out_eq1}");
-    println!("Meas = {Meas}");
-    println!("fac = {fac}");
-
-    println!("Out[init].Uc() = {}", Out_eq1.Eps());
-    println!("Out[init].Uc() = {}", Out_eq2.Eps());
-
-    for NumIteration in 0..100
-    {
-        Meas = sg_square(periode_square, <i16 as Into<f64>>::into(NumIteration) * Te);
-
-        Out_eq1 = ((1.0_f64 - fac.clone()) * Out_eq1_zm1.clone()) + (fac.clone() *  Meas.clone()                       ); // equation 1
-        Out_eq2 = (                          Out_eq2_zm1.clone()) + (fac.clone() * (Meas.clone() - Out_eq2_zm1.clone())); // equation 2
+	let Te = 0.04_f64; // 40ms
+	let Tf = 1.0_f64; // 1sec
+	let fac: RMesure = RMesure::from(1.0_f64 - (-2.0_f64 * 3.14159_f64 * Te/Tf).exp());
 
 
-        println!("[{NumIteration}]\t-->\t{Meas}\t{Out_eq1_zm1}");
-        println!("[{NumIteration}]\t-->\t{Meas}\t{Out_eq2_zm1}");
-        println!();
+	println!("Out = {Out_eq1}");
+	println!("Meas = {Meas}");
+	println!("fac = {fac}");
 
-        Out_eq1_zm1 = Out_eq1.clone();
-        Out_eq2_zm1 = Out_eq2.clone();
-    }
+	println!("Out[init].Uc() = {}", Out_eq1.Eps());
+	println!("Out[init].Uc() = {}", Out_eq2.Eps());
+
+	for NumIteration in 0..100
+	{
+		Meas = sg_square(periode_square, <i16 as Into<f64>>::into(NumIteration) * Te);
+
+		Out_eq1 = ((1.0_f64 - fac.clone()) * Out_eq1_zm1.clone()) + (fac.clone() *  Meas.clone()                       ); // equation 1
+		Out_eq2 = (                          Out_eq2_zm1.clone()) + (fac.clone() * (Meas.clone() - Out_eq2_zm1.clone())); // equation 2
+
+
+		println!("[{NumIteration}]\t-->\t{Meas}\t{Out_eq1_zm1}");
+		println!("[{NumIteration}]\t-->\t{Meas}\t{Out_eq2_zm1}");
+		println!();
+
+		Out_eq1_zm1 = Out_eq1.clone();
+		Out_eq2_zm1 = Out_eq2.clone();
+	}
 }
 
 fn test_incertitude_U_egal_RI()
 {
-    let fc: RMesure = 250_000_f64.into();
-    let Ct: RMesure = RMesure::loi(0.000_000_001_f64, 20.0_f64,'P');
+	let fc: RMesure = 250_000_f64.into();
+	let Ct: RMesure = RMesure::loi(0.000_000_001_f64, 10.0_f64,'P');
 
-    let Rt: RMesure = 1.0_f64 / (fc.clone() * Ct.clone());
 
-    let trois: RMesure = RMesure::loi(27.0_f64, 1.0_f64, 'R');
+	let Rt: RMesure = 1.0_f64 / (fc.clone() * Ct.clone());
 
-    println!("fc = {fc}");
-    println!("Ct = {Ct}");
-    println!("Rt = {Rt}");
 
-    let R1: RMesure = RMesure::loi(Rt.Val(), 5.0,'P');
-    println!("R1 = {R1}");
+	println!("fc = {fc}");
+	println!("Ct = {Ct}");
+	println!("Rt = {Rt}");
 
-    let ft: RMesure = 1.0_f64 / (R1.clone() * Ct.clone());
-    println!("ft = {ft}");
+	let R1: RMesure = RMesure::loi(Rt.Val(), 5.0,'P');
+	println!("R1 = {R1}");
 
-    println!("Rt.log10() = {}", Rt.clone().log10());
-    println!("trois = {}", trois.cbrt().log2());
+	let ft: RMesure = 1.0_f64 / (R1.clone() * Ct.clone());
+	println!("ft = {ft}");
 }
 
 fn main()
 {
-    //test_incertitude_filtrage_prem_ordre();
-    test_incertitude_U_egal_RI();
+	//test_incertitude_filtrage_prem_ordre();
+	test_incertitude_U_egal_RI();
 }
